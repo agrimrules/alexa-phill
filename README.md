af9eaab1490b662f0b8a24b809426989dda51943
Agrim Asthana <dev@agrimasthana.com>
2017-03-23 01:43:54 -0500

    added AFINN-165 based sentiment analysis

--- README.md
+++ README.md
@@ -5,9 +5,11 @@ A sample Alexa Skill that is packaged into a lambda function,
 This app is just a proof of concept and will probably evolve into something completely different over time.
 
 ### Changelog
+v0.3.0: Adding the ability to Analyze speech sentiment
 v0.2.1: Fixing Indent in `README.md`   
 v0.2.0: Added RepeatIntent to basically repeat what it is told   
 v0.1.0: Initial release with the `cash me outside easter egg`
-### Credit
+### Credits
 
  * [alexa-app](https://github.com/alexa-js/alexa-app)
+ * [sentiment] (https://github.com/thisandagain/sentiment)
--- phill.js
+++ phill.js
@@ -1,5 +1,6 @@
 var alexa = require("alexa-app");
 var app = new alexa.app('phill');
+var sentiment = require('sentiment');
 
 app.launch((req,res)=>{
   res.say("cash me outside howbow deh?");
@@ -26,6 +27,20 @@ app.intent('RepeatIntent',{
   ]
 }, (req, res)=>{
   res.say(`${req.slot('WORD')}`)
+});
+
+app.intent('AnalyzeIntent',{
+  'slots': {
+    'SENTIMENT': 'LITERAL'
+  },
+  'utterances':[
+    'to analyze {-|SENTIMENT}',
+    'to process {-|SENTIMENT}',
+    'to evaluate {-|SENTIMENT}'
+  ]
+},(req,res)=>{
+  sent = Math.round((sentiment(req.slot('SENTIMENT')).comparative / 5) * 100);
+  res.say(Math.abs(sent) + ' %' + (sent >=0 ? ' positive': ' negative'));
 });
 
 exports.handler = app.lambda();
