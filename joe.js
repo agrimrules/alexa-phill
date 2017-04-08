a1b906c14eba2e954d7b2cc6c80579d8624c5463
Agrim Asthana <dev@agrimasthana.com>
2017-04-08 00:27:11 -0500

    ported sentiment analyzer

--- joe.js
+++ joe.js
@@ -2,6 +2,7 @@
 
 var Alexa = require('alexa-sdk');
 var twilio = require('twilio');
+var sentiment = require('sentiment');
 var client = new twilio.RestClient(process.env.SID, process.env.TOKEN);
 
 exports.handler = function(event, context, callback) {
@@ -29,6 +30,10 @@ var handlers = {
         self.emit(':tell','Something went wrong');
       }
     })
+  },
+  'AnalyzeIntent': function () {
+    var s = Math.round((sentiment(this.event.request.intent.slots.SENTIMENT.value).comparative / 5) * 100);
+    this.emit(':tell', Math.abs(s)+' %' + (s >=0 ? ' positive': ' negative'));
   },
   'Unhandled': function () {
        self.emit(':tell', 'Soon', 'Cant do that');
