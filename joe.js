d8079f0a3668bbf5c9442b36b046388730d3e337
Agrim Asthana <dev@agrimasthana.com>
2017-09-25 23:20:12 -0500

    Initial 2FA implementation

--- joe.js
+++ joe.js
@@ -3,7 +3,9 @@
 var Alexa = require('alexa-sdk');
 var twilio = require('twilio');
 var sentiment = require('sentiment');
+var speakeasy = require('speakeasy');
 var client = new twilio.RestClient(process.env.SID, process.env.TOKEN);
+var token = process.env.two_fa_token;
 
 exports.handler = function(event, context, callback) {
   var alexa = Alexa.handler(event, context);
@@ -34,6 +36,24 @@ var handlers = {
   'AnalyzeIntent': function () {
     var s = Math.round((sentiment(this.event.request.intent.slots.SENTIMENT.value).comparative / 5) * 100);
     this.emit(':tell', Math.abs(s)+' %' + (s >=0 ? ' positive': ' negative'));
+  },
+  'VerifyIntent': function(){
+    console.log('Access-token:'+token);
+    console.log('prompt:'+this.event.request.intent.slots.CODE.value);
+      if(speakeasy.totp.verify({
+        secret: token,
+        encoding: 'base32',
+        token: this.event.request.intent.slots.CODE.value
+      })){
+        this.emit(':tell', 'Access Granted')
+      }
+      else {
+        this.emit(':tell', 'Access Denied')
+      }
+  },
+  'AuthorizeIntent': function() {
+    this.handler.state == "req-auth";
+    this.emit(':ask', 'Please tell me your access code', 'Please repeat your access code');
   },
   'Unhandled': function () {
        self.emit(':tell', 'Soon', 'Cant do that');
